bdda000e74c70a8c7849b9bdee8da46bf0b92767
yunchan312 <phenomenal312@hufs.ac.kr>
2024-01-27 00:28:59 +0900

    1966

--- Simulation/1966.py
+++ Simulation/1966.py
@@ -9,23 +9,37 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
+t = int(input())
 
-N, target = map(int, input().split())
-docu = list(map(int, input().split()))
-dq = deque()
-for n in range(N):
-    dq.append(n)
-ans =[]
-for i in range(N):
-    for j in range(i+1, N):
-        if docu[dq[i]] < docu[dq[j]]:
-            print('did')
-            temp = dq.popleft()
-            ans.append(temp)
-        print(dq)
+for _ in range(t):
+    N, target = map(int, input().split())
+    docu = deque(map(int, input().split()))
+    curPrt = 1
+    ind = deque(i for i in range(N))
 
+    '''프린트 할 때 해당 ind가 target과 같은지 검사하는 함수'''
+    def isTarget(ind):
+        if ind[0] == target:
+            return True
+        return False
 
-for d in dq:
-    if d == target:
-        print(ans)
-    ans +=1
+    '''현재 우선순위에 맞는건지 확인하는 함수'''
+    def tryPrint(docu):
+        if max(docu) == docu[0]:
+            return True
+        return False
+
+    while(ind):
+        if not tryPrint(docu):
+            indTemp = ind.popleft()
+            ind.append(indTemp)
+            docuTemp = docu.popleft()
+            docu.append(docuTemp)
+        else:
+            if isTarget(ind):
+                print(curPrt)
+                break
+            else:
+                curPrt +=1
+                ind.popleft()
+                docu.popleft()
