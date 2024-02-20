f0bf3bb2d13e592bd88995e0a3bf7b7d50785f49
yunchan123 <phenomenal312@hufs.ac.kr>
2024-02-20 20:37:05 +0900

    binarySearch

--- Binarysearch/14426.py
+++ Binarysearch/14426.py
@@ -1,12 +1,16 @@
 '''접두사 찾기'''
 import sys
 input = sys.stdin.readline
-from collections import deque
 
 N, M = map(int, input().split())
 strings = [input().strip() for _ in range(N)]
-texts = deque()
-for _ in range(M):
-    texts.append(input().strip())
+texts = [input().strip() for _ in range(M)]
+cnt = 0
 
-
+for text in texts:
+    for string in strings:
+        if string[0] == text[0]:
+            if text in string:
+                cnt +=1
+                break
+print(cnt)
